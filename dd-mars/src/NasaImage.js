285586851efceae19a2c23c6000d7855e319189f
Andreas Modahl <andreas.modahl@websystemer.as>
2021-03-25 11:34:31 +0100

    show random image instead of same one

--- dd-mars/src/NasaImage.js
+++ dd-mars/src/NasaImage.js
@@ -1,12 +1,37 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+function formatDate(date) {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+}
+
+const getRandomDate = () => {
+  const start = new Date("2014-01-01").getTime();
+  const end = new Date("2021-01-01").getTime();
+
+  const range = start - end;
+
+  const r = Math.floor(range * Math.random());
+
+  return formatDate(new Date(start + r));
+};
+
 function NasaImage() {
   const [data, setData] = useState({});
 
   useEffect(async () => {
     const data = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=BmJk1VkfUuy8AYtbejScKfSfrOvBK01K2d5OWT3E`
+      "https://api.nasa.gov/planetary/apod" +
+        "?api_key=BmJk1VkfUuy8AYtbejScKfSfrOvBK01K2d5OWT3E" +
+        `&date=${getRandomDate()}`
     );
 
     const body = await data.json();
